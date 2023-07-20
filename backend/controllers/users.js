bd486f531ceea9ef1d5470d4c2f245efdd70fdef
Artur Shchekach <arturshchekatch@yandex.ru>
2023-07-20 23:39:44 +1000

    fix: change cookie options

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -121,8 +121,8 @@ const login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         secure: true,
-        sameSite: true,
-        domain: '.keeperofplaces.nomoredomains.xyz',
+        sameSite: 'None',
+        domain: 'keeperofplaces.nomoredomains.xyz'
       }).send({ message: 'Успешная авторизация' });
     })
     .catch(next);
