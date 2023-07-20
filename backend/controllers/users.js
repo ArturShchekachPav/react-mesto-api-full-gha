a6ef9e1ee46654455dd18c9f12f8ca9d1fcd9a99
Artur Shchekach <arturshchekatch@yandex.ru>
2023-07-21 01:37:39 +1000

    fix: change cookie options

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -119,6 +119,7 @@ const login = (req, res, next) => {
 
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
         secure: true,
         sameSite: 'None',
       }).send({ message: 'Успешная авторизация' });
